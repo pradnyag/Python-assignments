f0f9be97aa7e8c2bc1c5b1e0c6d7114f5707e63e
Prado <pgpradnyagade@gmail.com>
2020-05-09 16:09:28 +0530

    Attaching Image

--- send_mail.py
+++ send_mail.py
@@ -1,23 +1,30 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from PIL import Image
 
 username='pradorockss@gmail.com'
 password='prado@123'
 
 def send_mail(text= 'Email Body',subject='Hello World',from_email='Pradnya Gade <pradorockss@gmail.com>',to_emails=
-'vjrnjn@gmail.com'):
-    #assert isinstance(to_emails,list)
+None,):
+    assert isinstance(to_emails,list)
     msg = MIMEMultipart("alternative")
     msg['From']= from_email
-    msg['To']= to_emails
+    msg['To']= ", ".join(to_emails)
     msg['Subject']= subject
 
     txt_part= MIMEText(text,'plain')
     msg.attach(txt_part)
 
-    # html_part= MIMEText("<h1>This is working<h1>",'html')
-    # msg.attach(txt_part)
+    html_part= MIMEText("<h1>This is working</h1>",'html')
+    msg.attach(html_part)
+
+    img  = Image.open(r"C:\Users\Prado\Desktop\Projects\Practice\SMTP_module\image.jpg")  
+    
+    img_part=MIMEImage(img.tobytes(),'jpg')
+    msg.attach(img_part)
 
     msg_str= msg.as_string()
 
@@ -33,4 +40,4 @@ def send_mail(text= 'Email Body',subject='Hello World',from_email='Pradnya Gade 
     #     pass
 
 if __name__ == "__main__":
-    send_mail()
+    send_mail(to_emails=['pradorockss@gmail.com']) 
