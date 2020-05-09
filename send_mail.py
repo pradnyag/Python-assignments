6787f7834e185ceb4f9e01ad0da0663bbdc8b204
Prado <pgpradnyagade@gmail.com>
2020-05-09 21:54:14 +0530

    Final Attaching Image

--- send_mail.py
+++ send_mail.py
@@ -2,7 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
-from PIL import Image
 
 username='pradorockss@gmail.com'
 password='prado@123'
@@ -18,14 +17,32 @@ None,):
     txt_part= MIMEText(text,'plain')
     msg.attach(txt_part)
 
-    html_part= MIMEText("<h1>This is working</h1>",'html')
-    msg.attach(html_part)
 
-    img  = Image.open(r"C:\Users\Prado\Desktop\Projects\Practice\SMTP_module\image.jpg")  
+    #Attaching image 
+    img_path=r'C:\Users\Prado\Desktop\Projects\Practice\SMTP_module\image.jpg'
     
-    img_part=MIMEImage(img.tobytes(),'jpg')
+    img_part=MIMEImage(open(img_path,'rb').read(),'jpg',name='image')
+    img_part.add_header('Content-ID','animal image')
     msg.attach(img_part)
 
+
+    # Attaching html part
+
+    html_content= """\
+    <html>
+    <head></head>
+    <body>
+        <p>Hi!<br>
+        Here is the <a href="https://www.python.org">link</a> you wanted.<br>
+        <img src="{{url_for('static',filename=img_path)}} alt="Smiley face" width="100" height="100" align="middle" style="margin:0px 50px">
+    </body>
+    </html>
+    """
+
+    html_part= MIMEText(html_content,'html')
+    msg.attach(html_part)
+
+    
     msg_str= msg.as_string()
 
     #login to smtp server
